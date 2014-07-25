51d62ef8a5dae50e638d499e78e8c0e4e750b2a8
Doug Hoff <email@doughoff.com>
2014-07-25 16:53:48 -0500

    Ex. 19 Node: routing server

---
+++ node-routing/routing-server.js
@@ -0,0 +1,24 @@
+// routing-server.js
+var http = require('http');
+
+http.createServer(function(req,res){
+    // normalize url by removing querystring, optional
+    // trailing slash, and making it lowercase
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(path) {
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+            break;
+    }
+}).listen(8080);
+
+console.log('Server started on localhost:8080');
