2f7cce25cb90969650a8de779583633e93cba213
Doug Hoff <email@doughoff.com>
2014-07-25 17:09:23 -0500

    Ex 20 - Restify

--- node-routing/routing-server.js
+++ node-routing/routing-server.js
@@ -1,21 +1,27 @@
 // routing-server.js
 var http = require('http');
 
-http.createServer(function(req,res){
+http.createServer(function(req, res) {
     // normalize url by removing querystring, optional
     // trailing slash, and making it lowercase
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    switch(path) {
+    switch (path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
             res.end('Homepage');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
             res.end('About');
             break;
         default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
             res.end('Not Found');
             break;
     }
