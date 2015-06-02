acde9dcee5569f0b00796a3d1e84726bf7493a3f
Doug Hoff <email@doughoff.com>
2015-06-02 14:51:28 -0500

    text server update

--- node-text/text-server.js
+++ node-text/text-server.js
@@ -1,9 +1,11 @@
 // text-server.js
 var http = require('http');
+var hits = 0;
 http.createServer(function(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
     response.end('Hello, world!');
+    console.log('Received hit request #' + ++hits);
 }).listen(8080);
 console.log('I\'m ready to respond with text.');
